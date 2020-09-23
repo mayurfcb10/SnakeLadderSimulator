3a91787ea8cb699403f1aa30e6fcdef03575a683
Mayur <mayurzope555@gmail.com>
2020-09-23 21:33:39 +0530

    [Mayur]Added: Welcome to the Snake Ladder Simulator Problem

---
+++ SnakeLadderSimulator.java
@@ -0,0 +1,7 @@
+/* Welcome to the Snake Ladder Simulator Problem */
+
+public class SnakeLadderSimulator{
+	public static void main(String[] args){
+		System.out.println("Welcome to the Snake Ladder Simulator Problem");
+	}
+} 
